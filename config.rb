8a374a7741ecbc856648545ba9f65a7a0697cf88
Isaiah <isaiah@atpay.com>
2015-06-19 04:40:38 -0600

    Initial Commit

---
+++ config.rb
@@ -0,0 +1,100 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+helpers do
+  def nav_link(link_text, url, options = {})
+    options[:class] ||= ""
+    options[:class] << " active" if url == current_page.url
+    link_to(link_text, url, options)
+  end
+
+
+  def tab_block(type, title, nav, block)
+    li_nav = ""
+    css_id = "A"
+
+    nav.each do |n|
+      li_nav =  li_nav + "<li class='#{n.split(/[\W_]/).map {|c| c.capitalize}.join}'><a href='##{n.split(/[\W_]/).map {|c|c.capitalize}.join}'>#{n}</a></li>"
+    end
+
+    blocks = ""
+
+    block.each_index do |i|
+      blocks =  blocks + "<div class='tab_cont #{css_id}' id='#{nav[i].split(/[\W_]/).map {|c| c.capitalize}.join}' >"+ block[i] +"</div>"
+      css_id = css_id.succ
+    end
+
+   "<div class='tabs #{type}'><h3 class='tab_title'>"+title+"</h3><ul class='lang-nav'>#{li_nav}</ul>" + blocks + "</div>"
+  end
+
+end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+set :haml, { :ugly => true, :format => :html5 }
+
+activate :syntax, :line_numbers => true
+
+
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
