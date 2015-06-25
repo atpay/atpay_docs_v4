7d8eee160b4231ae9fa95d45c50130fc2d815e86
Isaiah <isaiah@atpay.com>
2015-06-25 10:54:51 -0600

    Adds CNAME to souce folder

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem "middleman", "~>3.3.12"
 gem "middleman-livereload", "~> 3.1.0"
 
 
+gem 'middleman-deploy', '~> 1.0'
+
 gem 'middleman-syntax'
 
 # For faster file watcher updates on Windows:
--- config.rb
+++ config.rb
@@ -81,6 +81,16 @@ activate :syntax, :line_numbers => true
 
 
 
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -93,7 +103,7 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
